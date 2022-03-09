2831a795edb61a608686206e42d19b2eb5d23325
Thiago Andrade Developer <thiagoha3@gmail.com>
2022-03-09 17:24:09 -0300

    v1.0.1

--- FileAssistent.java
+++ FileAssistent.java
@@ -1,6 +1,13 @@
+//-------------------------------------------------------------
+// FileAssistent - v1.0
+// Licença: MIT
+//-------------------------------------------------------------- 
+
 package siscaixaadmin.FileConfig;
 
 import java.awt.Component;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -8,20 +15,56 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
+import java.lang.String;
 
-/**
- * @author Thiago Henrique de Andrade
- */
 public class FileAssistent {
-    
+
+    //método para compactar arquivo
+    public static void compactarParaZip(String arqSaida,String arqEntrada)
+    throws IOException{
+        
+        
+            //Constantes
+            int TAMANHO_BUFFER = (int) sizeFile(arqEntrada);
+            int cont;
+            byte[] dados = new byte[TAMANHO_BUFFER];
+
+            BufferedInputStream origem = null;
+            FileInputStream streamDeEntrada = null;
+            FileOutputStream destino = null;
+            ZipOutputStream saida = null;
+            ZipEntry entry = null;
+
+            try {
+                destino = new FileOutputStream(new File(arqSaida));
+                saida = new ZipOutputStream(new BufferedOutputStream(destino));
+                File file = new File(arqEntrada);
+                streamDeEntrada = new FileInputStream(file);
+                origem = new BufferedInputStream(streamDeEntrada, TAMANHO_BUFFER);
+                entry = new ZipEntry(file.getName());
+                saida.putNextEntry(entry);
+
+                while((cont = origem.read(dados, 0, TAMANHO_BUFFER)) != -1) {
+                    saida.write(dados, 0, cont);
+                }
+                origem.close();
+                saida.close();
+            } catch(IOException e) {
+                throw new IOException(e.getMessage());
+            }
+        }  
+
     //choose file
-    public File chooseOpenFile(String Address,String StrTitle){
+    public File chooseOpenFile(boolean dir, String Address,String StrTitle){
         
         String filename = File.separator+"tmp";
         JFileChooser fc = new JFileChooser(new File(filename));
         Component frame = null;
+
+        if(dir)fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY); 
         if(Address != null)fc.setCurrentDirectory(new File(Address));
         if(StrTitle != null)fc.setDialogTitle(StrTitle);
     
